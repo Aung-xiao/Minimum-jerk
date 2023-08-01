2a78f9c5fdd567263225193b390b35729ef00610
Aung-xiao <email_id>
2023-08-01 09:33:37 +0800

    delete useless head_file

--- waypoint_trajectory_generator/src/trajectory_generator_node.cpp
+++ waypoint_trajectory_generator/src/trajectory_generator_node.cpp
@@ -1,19 +1,11 @@
-#include <string>
-#include <iostream>
 #include <fstream>
 #include <math.h>
 #include <random>
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <nav_msgs/Path.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Point.h>
-#include <visualization_msgs/MarkerArray.h>
 #include <visualization_msgs/Marker.h>
-#include <quadrotor_msgs/PolynomialTrajectory.h>
-#include <sensor_msgs/Joy.h>
-#include <algorithm>
 
 // Useful customized headers
 #include "trajectory_generator_waypoint.h"
